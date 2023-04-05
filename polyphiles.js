630032a6b8c033d8402c5551b4e7355e5f784283
Antony Shaga <antonyshaga@gmail.com>
2023-04-05 19:29:54 +0300

    first commit

--- polyphiles.js
+++ polyphiles.js
@@ -87,3 +87,51 @@ Array.prototype.filter2 = function (callback, thisArg) {
 let prime = array.filter2(isPrime);
 
 console.log(prime);
+
+
+//Map
+//
+// let newArray = arr.map(callback(currentValue[, index[, array]]) {
+//     // return element for newArray, after executing something
+// }[, thisArg]);
+
+let array2 = [4, 6, 8, 9, 12, 53, -17, 2, 5, 7, 31, 97, -1, 17];
+
+Array.prototype.map2 = function (callback, thisArg) {
+  if (this == null) {
+    throw new Error("Cant iterate over undefined or null");
+  }
+  let context = this;
+
+  let O = Object(this);
+
+  if (arguments.length > 1) {
+    context = thisArg;
+  }
+
+  if (typeof callback !== "function") {
+    throw new Error("Callback is not a function");
+  }
+
+  let len = O.length;
+
+  let newArray = [];
+
+  let i = 0;
+
+  while (i < len) {
+    if (i in O) {
+      newArray[i] = callback.call(context, this[i], i, O);
+    }
+
+    i++;
+  }
+
+  return newArray;
+};
+
+let increase1 = (num) => num + 1;
+let mul2 = (num) => num * 2;
+
+console.log(array2.filter2(isPrime));
+console.log(array2.filter2(isPrime).map2(mul2).map2(increase1));
